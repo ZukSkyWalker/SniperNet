db807faf84351ae21c63dfadff014d116b2d9021
ZukSkyWalker <wzukai@gmail.com>
2023-09-05 17:17:04 -0700

    CUDA code skeleton

--- src/data_process/feature_map.py
+++ src/data_process/feature_map.py
@@ -1,14 +1,17 @@
 import numpy as np
+from numba import cuda
+from util.timing import timer_func
 
 
 class frame():
   def __init__(self, pos_arr, cfg) -> None:
+    self.cfg = cfg
     in_range  = (pos_arr[:, 0] > -cfg["x_range"]) & (pos_arr[:, 0] < cfg["x_range"])
-    in_range &= (pos_arr[:, 1] > -cfg["y_range"]) & (pos_arr[:, 0] < cfg["y_range"])
-    in_range &= (pos_arr[:, 2] >  cfg["z_min"]) & (pos_arr[:, 0] < cfg["z_max"])
+    in_range &= (pos_arr[:, 1] > -cfg["y_range"]) & (pos_arr[:, 1] < cfg["y_range"])
+    in_range &= (pos_arr[:, 2] >  cfg["z_min"]) & (pos_arr[:, 2] < cfg["z_max"])
     
     self.pos = pos_arr[in_range]
-    self.base = self.get_base(cfg)
+    self.base = self.get_base()
 
     # General shift
     self.pos[:, 2] -= self.base
@@ -16,57 +19,73 @@ class frame():
     self.height_map = np.zeros((cfg["NX"], cfg["NY"]))
     self.density_map = np.zeros((cfg["NX"], cfg["NY"]))
 
+  def load_point_cloud(self, pos_arr):
+    in_range  = (pos_arr[:, 0] > -self.cfg["x_range"]) & (pos_arr[:, 0] < self.cfg["x_range"])
+    in_range &= (pos_arr[:, 1] > -self.cfg["y_range"]) & (pos_arr[:, 1] < self.cfg["y_range"])
+    in_range &= (pos_arr[:, 2] >  self.cfg["z_min"]) & (pos_arr[:, 2] < self.cfg["z_max"])
+    
+    self.pos = pos_arr[in_range]
+    self.base = self.get_base()
 
-  def get_base(self, cfg):
-    cnts, edges = np.histogram(self.pos[:, 2], bins=np.arange(cfg["z_min"],cfg["z_max"],cfg["dz"]))
-    pts_thresh = max(len(self.pos) * cfg["min_ground_ratio"], cfg["min_ground_layer_pts"])
+    # General shift
+    self.pos[:, 2] -= self.base
+
+
+  def get_base(self):
+    cnts, edges = np.histogram(self.pos[:, 2], bins=np.arange(self.cfg["z_min"],self.cfg["z_max"],self.cfg["dz"]))
+    pts_thresh = max(len(self.pos) * self.cfg["min_ground_ratio"], self.cfg["min_ground_layer_pts"])
     base_idx = np.where(cnts > pts_thresh)[0][0]
     return edges[base_idx]
 
 
-  def get_h_map(self, z_arr, cfg):
-    # Notice the z distribution has already been shifted so the ground level is at around 0
-    cnts, edges = np.histogram(z_arr, bins=np.arange(0,cfg["z_max"] - cfg["z_min"],cfg["dz"]))
-    base = cfg["z_min"] - 1
-    height = base
-    gap = 0
-
-    for i in range(len(cnts)):
-      if cnts[i] > 0:
-        if base < cfg["z_min"]:
-          base = edges[i]
-        height = edges[i]
-        gap = 0
-
-      elif edges[i] > cfg["h_range"]:
-        gap += 1
-        if gap > cfg["max_z_gap"]:
-          # having a gap after high enough range
-          break
-
-    return base, height
-
- 
-
-  def set_bev_map_orig(self, cfg):
+  @timer_func
+  def set_bev_map_cpu(self):
     """
-    TODO:
-    Fill self.base_map and self.height_map using the get_h_map
+    Return base_map, height_map and the counts map as a supervisor
     """
     # Extract x, y, z coordinates
-    x, y, z = self.pos[:, 0], self.pos[:, 1], 
+    x, y = self.pos[:, 0], self.pos[:, 1]
 
     # Compute the grid indices for every point
-    idx_x = (0.5 * (x + cfg["x_range"]) * cfg["NX"] / cfg["x_range"]).astype(np.uint16)
-    idx_y = (0.5 * (y + cfg["y_range"]) * cfg["NY"] / cfg["y_range"]).astype(np.uint16)
+    idx_x = (0.5 * (x + self.cfg["x_range"]) * self.cfg["NX"] / self.cfg["x_range"]).astype(np.int32)
+    idx_y = (0.5 * (y + self.cfg["y_range"]) * self.cfg["NY"] / self.cfg["y_range"]).astype(np.int32)
 
-    for gx in range(cfg['NX']):
-      for gy in range(cfg["NY"]):
-        in_grid = (gx == idx_x) & (gy == idx_y)
-        b, h = self.get_h_map(self.pos[in_grid, 2])
-        self.base_map[gx, gy] = b
-        self.height_map[gx, gy] = h
+    glb_idx = idx_x * self.cfg["NY"] + idx_y
 
+    base_map = np.zeros((self.cfg["NX"], self.cfg["NY"]))
+    height_map = np.zeros((self.cfg["NX"], self.cfg["NY"]))
+    density_map = np.zeros((self.cfg["NX"], self.cfg["NY"]))
+
+    sorted_indices = np.lexsort((glb_idx, self.pos[:, 2]))
+    
+    i0 = 0
+    z = self.pos[sorted_indices, 2]
+    unique_idx, idx_counts = np.unique(glb_idx[sorted_indices], return_counts=True)
+
+    for i in range(len(unique_idx)):
+      z_arr = z[i0:(i0+idx_counts[i])]
+      # Decode the grid index
+      gx = unique_idx[i] // self.cfg["NY"]
+      gy = unique_idx[i] % self.cfg["NY"]
+
+      # Set the grid height and base
+      base_map[gx, gy] = z_arr.min()
+
+      h = z_arr.max()
+
+      if len(z_arr) > 1:
+        idx_arr = np.where((z_arr[1:] > z_arr[:-1] + self.cfg["max_z_gap"]) & (z_arr[1:] > self.cfg["sig_height_range"]))[0]
+        if len(idx_arr) > 0:
+          h = z_arr[idx_arr[0]]
+
+      height_map[gx, gy] = h
+      density_map[gx, gy] = idx_counts[i]
+      i0 += idx_counts[i]
+
+    return base_map, height_map, density_map
+
+
+  @timer_func
   def set_bev_map(self, cfg):
     # Extract x, y, z coordinates
     x, y, z = self.pos[:, 0], self.pos[:, 1], self.pos[:, 2]
@@ -95,3 +114,52 @@ class frame():
       self.base_map[gx, gy] = b
       self.height_map[gx, gy] = h
 
+
+@cuda.jit
+def bev_map_kernel(pos, x_range, y_range, NX, NY, max_z_gap, sig_height_range, base_map, height_map, density_map):
+    tx = cuda.threadIdx.x  # Thread id in a 1D block
+    ty = cuda.blockIdx.x   # Block id in a 1D grid
+    bw = cuda.blockDim.x   # Block width, i.e. number of threads per block
+    pos_idx = tx + ty * bw
+
+    if pos_idx >= pos.shape[0]:  # Ensure we don't go out of bounds
+        return
+
+    x, y, z = pos[pos_idx]
+
+    # Compute the grid indices for the point
+    idx_x = int(0.5 * (x + x_range) * NX / x_range)
+    idx_y = int(0.5 * (y + y_range) * NY / y_range)
+
+    # Atomic operations ensure that updates from multiple threads are serialized
+    cuda.atomic.min(base_map, (idx_x, idx_y), z)
+    cuda.atomic.max(height_map, (idx_x, idx_y), z)
+    cuda.atomic.add(density_map, (idx_x, idx_y), 1)  # Increment count by 1
+
+def set_bev_map_gpu(self):
+    # ... Preparation of data ...
+
+    # Memory allocation and data transfer to device
+    pos_device = cuda.to_device(self.pos)
+
+    base_map_device = cuda.device_array((self.cfg["NX"], self.cfg["NY"]), dtype=np.float32)
+    height_map_device = cuda.device_array((self.cfg["NX"], self.cfg["NY"]), dtype=np.float32)
+    density_map_device = cuda.device_array((self.cfg["NX"], self.cfg["NY"]), dtype=np.int32)
+
+    # Initialize base_map_device to a very large value for the atomic.min operation
+    base_map_device[:] = np.finfo(np.float32).max
+
+    # Initialize height_map_device to a very small value for the atomic.max operation
+    height_map_device[:] = np.finfo(np.float32).min
+
+    # Launching the kernel
+    threadsperblock = 32
+    blockspergrid = (self.pos.shape[0] + (threadsperblock - 1)) // threadsperblock
+    bev_map_kernel[blockspergrid, threadsperblock](pos_device, self.cfg["x_range"], self.cfg["y_range"], self.cfg["NX"], self.cfg["NY"], self.cfg["max_z_gap"], self.cfg["sig_height_range"], base_map_device, height_map_device, density_map_device)
+
+    # Copy data back from device to host
+    base_map = base_map_device.copy_to_host()
+    height_map = height_map_device.copy_to_host()
+    density_map = density_map_device.copy_to_host()
+
+    return base_map, height_map, density_map
